ccd1cc490c13be91c8d13a07aa5c6a5da74083f2
Raul Diamantino <rauldiamantino25@gmail.com>
2023-10-28 03:46:06 -0300

    Fix: Corrige estrutura de pastas

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-require_once '../Routes/Routes.php';
-require_once '../config/config.php';
+require_once '../app/Routes/Routes.php';
+require_once '../app/config/config.php';
 require_once '../app/Controllers/PrayersController.php';
 require_once '../app/Validations/Validator.php';
 
